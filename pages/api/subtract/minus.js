c50cc3c1079438a310b09899b6a521f154570ade
riley-han <rileyhansen29@gmail.com>
2022-05-16 23:36:58 -0600

    created simple api for four main math fn

---
+++ pages/api/subtract/minus.js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+  const { num1, num2 } = req.query
+
+  const result = parseInt(num1) - parseInt(num2)
+
+  res.status(200).json({ result: `${result}` })
+}
